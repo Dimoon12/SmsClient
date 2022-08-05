5322e6f890d72fca6f8532772325e77ad172b8b5
Dimoon12 <dmitry.ryj@gmail.com>
2022-08-05 16:49:18 +0400

    First commit

---
+++ client.py
@@ -0,0 +1,21 @@
+import requests
+
+token = "db2bb923a29979184f454582fde8012653f6cc4c"
+
+while 1 == 1:
+
+    number = input("Номер:")
+    while True:
+        message = input("(YOU)")
+        if message[0] == '/':
+            if message == '/exit':
+                break
+            elif message == '/help':
+                print("/exit - выход")
+                print("/help - помощь")
+            else:
+                print("Неизвестная команда")
+        else:
+            resp=requests.get(f"http://[201:a65e:753f:842e:d958:8487:edc7:ef9e]:8080/{message}@{number}@{token}", timeout=5)
+            if not resp.text == "OK":
+                print(f"Ошибка: {resp.text}")

6939a90d80616e5aea4ef633c52b9f2679631131
Dimoon12 <dmitry.ryj@gmail.com>
2022-08-10 20:05:55 +0400

    Режим повторных попыток при сбое отправки

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import requests
 #import token from file
+messagerepeater=False
 with open("token.txt", "r") as f:
     token = f.read()
 while True:
@@ -19,3 +20,15 @@ while True:
             resp=requests.get(f"http://[201:a65e:753f:842e:d958:8487:edc7:ef9e]:8080/{message}@{number}@{token}", timeout=10)
             if not resp.text == "OK":
                 print(f"Ошибка: {resp.text}")
+                # сделать реакию на задокументированные ошибки а не только на сбой сети
+                if messagerepeater == True:
+                    while True:
+                        try:
+                          requests.get(f"http://[201:a65e:753f:842e:d958:8487:edc7:ef9e]:8080/{message}@{number}@{token}",timeout=10)
+                        except:
+                            print("Ошибка повторной отправки, пытаюсь снова")
+                        else:
+                            print("Повторная отправка прошла успешно")
+                            break
+                    break
+

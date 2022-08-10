8a28f242cdca5977cba38ec7a4d00bea3240ca49
Dimoon12 <dmitry.ryj@gmail.com>
2022-08-10 21:23:42 +0400

    Режим повторных попыток при сбое отправки (fix) и изменения в документации и сервере (Новая ошибка)

--- client.py
+++ client.py
@@ -2,11 +2,10 @@ import time
 
 import requests
 #import token from file
-messagerepeater=False
+messagerepeater=True
 with open("token.txt", "r") as f:
     token = f.read()
 while True:
-
     number = input("Номер:")
     while True:
         message = input("(YOU)")
@@ -22,14 +21,13 @@ while True:
             resp=requests.get(f"http://[201:a65e:753f:842e:d958:8487:edc7:ef9e]:8080/{message}@{number}@{token}", timeout=10)
             if not resp.text == "OK":
                 print(f"Ошибка: {resp.text}")
-                # тест
                 if messagerepeater == True:
                     while True:
                         reqloop=requests.get(f"http://[201:a65e:753f:842e:d958:8487:edc7:ef9e]:8080/{message}@{number}@{token}",timeout=10)
                         if reqloop.text == "OK":
-                            print("Finnally sent")
+                            print("Отправлено")
                             break
                         else:
-                            print(f"Ошибка, пытаемся снова")
+                            print(f"Ошибка, пытаемся снова ({reqloop.text})")
                             time.sleep(3)
 

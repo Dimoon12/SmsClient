78c97cec5b44d90364a4b557c1e0be5b967a0ff4
Dimoon12 <dmitry.ryj@gmail.com>
2022-08-25 22:55:28 +0400

    Обработка ошибок и ссылки на вики

--- client.py
+++ client.py
@@ -1,11 +1,36 @@
 import time
 
 import requests
+
+#######
+# Переключите на true и вам будут совать ссылки на справку по ошибке которая выскочила
+linkon = False
+#######
+
+def fault(fault,type):
+    if type == "CRIT":
+        print(f"[КРИТ] Программа будет закрыта Т.К восстановить работоспособность не удалось. Ошибка: ({fault})")
+        if linkon == True:
+            print(f"https://github.com/Dimoon12/SmsClient/wiki/{fault}")
+        exit()
+    elif type == "WARN":
+        print(f"[ПРЕД] Предупреждение, проверьтие проблему ({fault}). продолжаем работу")
+        if linkon == True:
+            print(f"https://github.com/Dimoon12/SmsClient/wiki/{fault}")
+
+def checkinet():
+    try:
+        requests.get("http://[21e:a51c:885b:7db0:166e:927:98cd:d186]", timeout=4)
+        return True
+    except:
+        fault("Yggdrasil_unavaible", "CRIT")
+
 #import token from file
 messagerepeater=True
 with open("token.txt", "r") as f:
     token = f.read()
 while True:
+    checkinet()
     number = input("Номер:")
     while True:
         message = input("(YOU)")

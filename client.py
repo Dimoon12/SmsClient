c287b992fb4fb442a8170b070bb8d254c3591a2e
Dimoon12 <dmitry.ryj@gmail.com>
2022-08-26 15:57:47 +0400

    Изменение логики повторной отправки сообщений, интеграция новой системы обработки ошибок

--- client.py
+++ client.py
@@ -1,33 +1,33 @@
 import time
 import requests
 
-#######
+###
 # Переключите на True и вам будут совать ссылки на справку по ошибке которая выскочила
 linkon = False
+####
 
-
-def fault(failurecode, errortype):
+# обработка ошибок
+def fault(failuretext, errortype, errcode):
     if errortype == "CRIT":
-        print(f"[КРИТ] Программа будет закрыта Т.К восстановить работоспособность не удалось. Ошибка: ({failurecode})")
+        print(f"[КРИТ] Программа будет закрыта Т.К восстановить работоспособность не удалось. ({failuretext})")
         if linkon:
-            print(f"https://github.com/Dimoon12/SmsClient/wiki/{failurecode}")
+            print(f"https://github.com/Dimoon12/SmsClient/wiki/{errcode}")
         exit()
     elif errortype == "WARN":
-        print(f"[ПРЕД] Предупреждение, проверьтие проблему ({failurecode}). продолжаем работу")
+        print(f"[ПРЕД] Предупреждение, {failuretext}")
         if linkon:
-            print(f"https://github.com/Dimoon12/SmsClient/wiki/{failurecode}")
+            print(f"https://github.com/Dimoon12/SmsClient/wiki/{errcode}")
 
-
+# проверка яжки и инета
 def checkinet():
     try:
         requests.get("http://[21e:a51c:885b:7db0:166e:927:98cd:d186]", timeout=4)
         return True
     except Exception:
-        fault("Yggdrasil_unavaible", "CRIT")
+        fault("сетевой Yggdrasil сервис недоступен", "CRIT", "Yggdrasil_unavaible")
 
 
 # import token from file
-messagerepeater = True
 with open("token.txt", "r") as f:
     token = f.read()
 while True:
@@ -44,18 +44,16 @@ while True:
             else:
                 print("Неизвестная команда")
         else:
-            resp = requests.get(f"http://[201:a65e:753f:842e:d958:8487:edc7:ef9e]:8080/{message}@{number}@{token}",
-                                timeout=10)
+            resp = requests.get(f"http://[201:a65e:753f:842e:d958:8487:edc7:ef9e]:8080/{message}@{number}@{token}", timeout=10)
             if not resp.text == "OK":
-                print(f"Ошибка: {resp.text}")
-                if messagerepeater:
-                    while True:
-                        reqloop = requests.get(
-                            f"http://[201:a65e:753f:842e:d958:8487:edc7:ef9e]:8080/{message}@{number}@{token}",
-                            timeout=10)
-                        if reqloop.text == "OK":
-                            print("Отправлено")
-                            break
-                        else:
-                            print(f"Ошибка, пытаемся снова ({reqloop.text})")
-                            time.sleep(3)
+                 while True:
+                    reqloop = requests.get(f"http://[201:a65e:753f:842e:d958:8487:edc7:ef9e]:8080/{message}@{number}@{token}", timeout=10)
+                    if reqloop.text == "OK":
+                        print("Отправлено")
+                        break
+                    elif reqloop.text == "F_ASS_DIED":
+                        fault(f"Ошибка сервера, повторяем попытку", "WARN", f"{reqloop.text}")
+                        time.sleep(3)
+                    else:
+                        fault(f"проверьте ввод ({reqloop.text})", "WARN", f"{reqloop.text}")
+                        break

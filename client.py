cdeb832c4d6cb9b623a1f837e1f88f35d9ee8704
Dimoon12 <dmitry.ryj@gmail.com>
2022-08-28 00:17:57 +0400

    Переписано, не тестировано

--- client.py
+++ client.py
@@ -1,11 +1,42 @@
-import time
+import asyncio
 import requests
+from aioconsole import ainput
 
-version = "0.1.1"
+numbers = []
+version = "1.0.0"
 ###
 # Переключите на True и вам будут совать ссылки на справку по ошибке которая выскочила
 linkon = False
+
 ####
+
+# import token from file
+with open("token.txt", "r") as f:
+    token = f.read()
+
+
+def response(serverresponse):
+    if serverresponse == "OK":
+        print("Отправлено")
+        return "ok"
+    elif serverresponse == "F_ASS_DIED":
+        fault("Ошибка отправки, пытаюсь повторно", "DEBG", "F_ASS_DIED")
+        return "repeat"
+    else:
+        fault(f"Ошибка отправки. Прерываюсь", "WARN", f"{serverresponse}")
+        return "fail"
+
+
+# обработка команд
+def command(command):
+    if command == "/exit":
+        return False
+    elif command == "/help":
+        print("/exit - ввод номера")
+        print("/help - помощь")
+    else:
+        print("Неизвестная команда")
+
 
 # обработка ошибок
 def fault(failuretext, errortype, errcode):
@@ -19,6 +50,7 @@ def fault(failuretext, errortype, errcode):
         if linkon:
             print(f"https://github.com/Dimoon12/SmsClient/wiki/{errcode}")
 
+
 # проверка яжки и инета
 def checkinet():
     try:
@@ -28,35 +60,33 @@ def checkinet():
         fault("сетевой Yggdrasil сервис недоступен", "CRIT", "Yggdrasil_unavaible")
 
 
-# import token from file
-with open("token.txt", "r") as f:
-    token = f.read()
+async def sendtask():
+    await asyncio.sleep(5)
+    print("loop running")
+    if numbers != []:
+        tempnumber = numbers[0].split(":")
+        number = tempnumber[0]
+        message = tempnumber[1]
+        responseraw = requests.get(f"http://[201:a65e:753f:842e:d958:8487:edc7:ef9e]:8080/{message}@{number}@{token}",
+                                   timeout=2)
+        if response(responseraw.text) == "OK":
+            numbers.remove(numbers[0])
+            print(f"Отправлено {message} на {number}")
 
-    ####### исправить этот говнокод
-while True:
-    checkinet()
-    number = input("Номер:")
-    while True:
-        message = input("(YOU)")
-        if message[0] == "/":
-            if message == "/exit":
-                break
-            elif message == "/help":
-                print("/exit - ввод номера")
-                print("/help - помощь")
-            else:
-                print("Неизвестная команда")
-        else:
-            resp = requests.get(f"http://[201:a65e:753f:842e:d958:8487:edc7:ef9e]:8080/{message}@{number}@{token}", timeout=10)
-            if not resp.text == "OK":
-                 while True:
-                    reqloop = requests.get(f"http://[201:a65e:753f:842e:d958:8487:edc7:ef9e]:8080/{message}@{number}@{token}", timeout=10)
-                    if reqloop.text == "OK":
-                        print("Отправлено")
-                        break
-                    elif reqloop.text == "F_ASS_DIED":
-                        fault(f"Ошибка сервера, повторяем попытку", "WARN", f"{reqloop.text}")
-                        time.sleep(3)
-                    else:
-                        fault(f"проверьте ввод ({reqloop.text})", "WARN", f"{reqloop.text}")
-                        break
+
+async def main():
+   while True:
+       exitloop = False
+       number = await ainput("Номер ")
+       while True:
+           if exitloop == True: break
+           exitloop = False
+           message = await ainput(">>> ")
+           checkinet()
+           if message[0] == "/" and command(message) == False:
+               exitloop = True
+               break
+           else:
+               numbers.append(f"{number}:{message}")
+asyncio.run(sendtask())
+asyncio.run(main())
